816c9755797ab296ea5bb161a032227ed6cb75ee
Gebo <gebso05@abv.bg>
2026-03-18 22:06:09 +0100

    changes to the design makeing it resposive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Projects from './components/Projects'
 import Header from './components/Header'
 import Showcase from './components/Showcase'
 import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <div>
